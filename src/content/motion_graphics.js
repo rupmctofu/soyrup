982108e7a50bc062d13796fbdea775a73ecd9dd0
iRup <irup@iMac-de-iRup.local>
2022-02-05 18:22:52 +0100

    falta diseno

--- src/content/motion_graphics.js
+++ src/content/motion_graphics.js
@@ -18,5 +18,69 @@ export const motion_graphics = {
         ]
       }
     },
+    {
+      slug: "motion-model-litoral",
+      date: "5/2/2022",
+      title: "Nou model de litoral - Ayuntamiento de Sitges",
+      description: "Vídeo Motion Graphics para Ayuntamiento de Sitges",
+      image: "/motion_graphics/litoral/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/LHWnjJVxXH0",
+        images: [
+          "/motion_graphics/litoral/1.jpg",
+          "/motion_graphics/litoral/2.jpg",
+          "/motion_graphics/litoral/3.jpg",
+          "/motion_graphics/litoral/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "motion-hpo",
+      date: "5/2/2022",
+      title: "Campaña Vivienda de Protección Oficial - Ayuntamiento de Sitges",
+      description: "Vídeo Motion Graphics para la campaña de Vivienda de Protección Oficial par el Ayuntamiento de Sitges",
+      image: "/motion_graphics/hpo/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/CxGBwzVWQUM",
+        images: [
+          "/motion_graphics/hpo/1.jpg",
+          "/motion_graphics/hpo/2.jpg",
+          "/motion_graphics/hpo/3.jpg",
+          "/motion_graphics/hpo/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "motion-menuplis",
+      date: "5/2/2022",
+      title: "Video Promocional - Menuplis",
+      description: "Vídeo Motion Graphics para la aplicación Menuplis",
+      image: "/motion_graphics/menuplis/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/yjQvMWtSXR8",
+        images: [
+          "/motion_graphics/menuplis/1.jpg",
+          "/motion_graphics/menuplis/2.jpg",
+          "/motion_graphics/menuplis/3.jpg",
+          "/motion_graphics/menuplis/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "motion-ocularis",
+      date: "5/2/2022",
+      title: "Vídeo Promocional - Ocularis ONGD",
+      description: "Vídeo Motion Graphics para Ocularis ONGD",
+      image: "/motion_graphics/ocularis/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/H1SJ0LVYemk",
+        images: [
+          "/motion_graphics/ocularis/1.jpg",
+          "/motion_graphics/ocularis/2.jpg",
+          "/motion_graphics/ocularis/3.jpg",
+          "/motion_graphics/ocularis/4.jpg",
+        ]
+      }
+    },
   ]
 }
