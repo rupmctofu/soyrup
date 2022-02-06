94c165105ce2dbdeb82cbce431ae8dab25cfa6b7
verdulife <verdu@live.com>
2022-02-06 22:42:34 +0100

    gmail

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,22 +1,16 @@
 import nodemailer from 'nodemailer';
 
 const account = {
-  user: "verdu@live.com",//"atontao@hotmail.com",
-  pass: "88Lifemail",//"5683Laugh",
-  to: "verdukactus@gmail.com"//"roberto.v.lopez@gmail.com"
+  user: "verdukactus@fgmail.com",//"atontao@hotmail.com",
+  pass: "bytrczyrhlcxlevv",//"5683Laugh",
+  to: "verdu@live.com"//"roberto.v.lopez@gmail.com"
 }
 
 export async function post({ request }) {
   const form = await request.json();
 
   let transporter = nodemailer.createTransport({
-    service: "Outlook365",
-    host: "smtp.office365.com",
-    port: "587",
-    tls: {
-      ciphers: "SSLv3",
-      rejectUnauthorized: false,
-    },
+    service: "Gmail",
     auth: {
       user: account.user,
       pass: account.pass,
