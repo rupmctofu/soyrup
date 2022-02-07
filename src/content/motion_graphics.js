aeacba75259897b30cceb14bcde99cd7f2b4267e
iRup <irup@iMac-de-iRup.local>
2022-02-07 19:29:43 +0100

    more projects

--- src/content/motion_graphics.js
+++ src/content/motion_graphics.js
@@ -82,5 +82,101 @@ export const motion_graphics = {
         ]
       }
     },
+    {
+      slug: "ajuts-lloguer",
+      date: "5/2/2022",
+      title: "Vídeo Ajuts al lloguer - Aj. de Sitges",
+      description: "Vídeo Motion Graphics para campaña de Ayudas al Alquiler del Ayuntamiento de Sitges",
+      image: "/motion_graphics/ajutslloguer/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/ufzZF9sFGBY",
+        images: [
+          "/motion_graphics/ajutslloguer/1.jpg",
+          "/motion_graphics/ajutslloguer/2.jpg",
+          "/motion_graphics/ajutslloguer/3.jpg",
+          "/motion_graphics/ajutslloguer/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "ajuts-reforma",
+      date: "2/2/2022",
+      title: "Vídeo Ajuts obtenció IAE - Aj. de Sitges",
+      description: "Vídeo Motion Graphics para campaña de Ayudas a la Rehabilitación arquitectónica y energética del Ayuntamiento de Sitges",
+      image: "/motion_graphics/ajutsreforma/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/WLsmZBjvBsI",
+        images: [
+          "/motion_graphics/ajutsreforma/1.jpg",
+          "/motion_graphics/ajutsreforma/2.jpg",
+          "/motion_graphics/ajutsreforma/3.jpg",
+          "/motion_graphics/ajutsreforma/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "solicitantes-hpo",
+      date: "2/2/2022",
+      title: "Vídeo Solicitantes HPO - Aj. de Sitges",
+      description: "Vídeo Motion Graphics para campaña de Ayudas a la vivienda del Ayuntamiento de Sitges",
+      image: "/motion_graphics/promocions/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/ialFKZw1h7I",
+        images: [
+          "/motion_graphics/promocions/1.jpg",
+          "/motion_graphics/promocions/2.jpg",
+          "/motion_graphics/promocions/3.jpg",
+          "/motion_graphics/promocions/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "lloguer-just",
+      date: "2/2/2022",
+      title: "Vídeo Alquiler Justo - Aj. de Sitges",
+      description: "Vídeo Motion Graphics para campaña de Alquiler Justo del Ayuntamiento de Sitges",
+      image: "/motion_graphics/lloguerjust/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/9TsXAp6T2JI",
+        images: [
+          "/motion_graphics/lloguerjust/1.jpg",
+          "/motion_graphics/lloguerjust/2.jpg",
+          "/motion_graphics/lloguerjust/3.jpg",
+          "/motion_graphics/lloguerjust/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "requisits-hpo",
+      date: "2/2/2022",
+      title: "Vídeo Requisitos Vivienda Protección Oficial - Aj. de Sitges",
+      description: "Vídeo Motion Graphics para campaña de Requisitos para optar a Vivienda de Protección Oficial del Ayuntamiento de Sitges",
+      image: "/motion_graphics/requisits/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/RCW8SfUb1-M",
+        images: [
+          "/motion_graphics/requisits/1.jpg",
+          "/motion_graphics/requisits/2.jpg",
+          "/motion_graphics/requisits/3.jpg",
+          "/motion_graphics/requisits/4.jpg",
+        ]
+      }
+    },
+    {
+      slug: "plec",
+      date: "2/2/2022",
+      title: "Vídeo Plec clàusules - Aj. de Sitges",
+      description: "Vídeo Motion Graphics para el nuevo modelo de litoral del Ayuntamiento de Sitges",
+      image: "/motion_graphics/plec/preview.jpg",
+      content: {
+        mockup: "https://www.youtube.com/embed/L1F09ipIUGY",
+        images: [
+          "/motion_graphics/plec/1.jpg",
+          "/motion_graphics/plec/2.jpg",
+          "/motion_graphics/plec/3.jpg",
+          "/motion_graphics/plec/4.jpg",
+        ]
+      }
+    },
   ]
 }
