0898a6609e8acca6673b516afda65532e1640cf2
verdulife <verdu@live.com>
2022-02-08 15:51:24 +0100

    video bg

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,8 +7,10 @@
 <main>
   <Nav />
 
-  <div class="bg-grad-1 row fill" />
-  <div class="bg-grad-2 row fill" />
+  <div class="video-wrapper fill">
+    <div class="overlay fill" />
+    <video playsinline autoplay loop muted src="/bg.mp4" class="fill" />
+  </div>
 
   <div class="scroll fill">
     <slot />
@@ -25,107 +27,21 @@
     }
   }
 
-  .scroll {
-    background: url("../../../dots.svg");
-    background-position: top left;
-    animation: moveBg 20s ease infinite alternate;
-  }
-
-  @mixin dots($count) {
-    $text-shadow: ();
-    @for $i from 0 through $count {
-      $text-shadow: $text-shadow,
-        (-0.5+ (random()) * 3) + em (-0.5+ (random()) * 1) + em 7px hsla(random() * 150, 50%, 50%, 0.9);
-    }
-    text-shadow: $text-shadow;
-  }
-
-  .bg-grad-1,
-  .bg-grad-2 {
+  .video-wrapper {
     position: fixed;
-    top: 50%;
-    left: 50%;
-    font-size: 150px;
-    color: transparent;
+    top: 0;
+    left: 0;
 
-    @media (max-width: $tablet) {
-      font-size: 100px;
+    .overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      background: rgba($black, 0.7);
     }
 
-    @media (max-width: $mobile) {
-      font-size: 20px;
-    }
-  }
-
-  .bg-grad-2::before,
-  .bg-grad-2::after,
-  .bg-grad-1::before,
-  .bg-grad-1::after {
-    width: 3em;
-    height: 3em;
-    content: ".";
-    mix-blend-mode: screen;
-    animation: 44s -27s move infinite ease-in-out alternate;
-    opacity: 0.5;
-  }
-
-  .bg-grad-1::before {
-    @include dots(50);
-    animation-duration: 44s;
-    animation-delay: -27s;
-  }
-
-  .bg-grad-1::after {
-    @include dots(50);
-    animation-duration: 43s;
-    animation-delay: -32s;
-  }
-
-  .bg-grad-2::before {
-    @include dots(50);
-    animation-duration: 42s;
-    animation-delay: -23s;
-  }
-
-  .bg-grad-2::after {
-    @include dots(50);
-    animation-duration: 41s;
-    animation-delay: -19s;
-  }
-
-  @keyframes move {
-    from {
-      transform: rotate(0deg) scale(12) translateX(-20px);
-    }
-    to {
-      transform: rotate(360deg) scale(18) translateX(20px);
-    }
-  }
-
-  @keyframes moveBg {
-    0% {
-      background-position: top left;
-      background-size: 100%;
-    }
-    20% {
-      background-position: bottom right;
-      background-size: 200%;
-    }
-    40% {
-      background-position: center;
-      background-size: 100%;
-    }
-    60% {
-      background-position: center;
-      background-size: 300%;
-    }
-    80% {
-      background-position: center;
-      background-size: 200%;
-    }
-    100% {
-      background-position: top left;
-      background-size: 100%;
+    video {
+      object-fit: cover;
+      object-position: center;
     }
   }
 </style>
